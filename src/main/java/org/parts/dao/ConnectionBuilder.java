3d6f6c8238fbce657d36d426042d22a30765ed74
fathergeorge <vovathewombat@yandex.ru>
2018-05-05 07:01:53 +0300

    DB begin

---
+++ src/main/java/org/parts/dao/ConnectionBuilder.java
@@ -0,0 +1,31 @@
+package org.parts.dao;
+
+import org.h2.jdbcx.JdbcDataSource;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by fathergeorge on 05.05.18.
+ */
+public class ConnectionBuilder {
+    public static Connection getH2Connection() {
+        try {
+            String url = "jdbc:h2:./h2db";
+            String name = "tully";
+            String pass = "tully";
+
+            JdbcDataSource ds = new JdbcDataSource();
+            ds.setURL(url);
+            ds.setUser(name);
+            ds.setPassword(pass);
+
+            Connection connection = DriverManager.getConnection(url, name, pass);
+            return connection;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/main/java/org/parts/dao/PartsDAOImpl.java
+++ src/main/java/org/parts/dao/PartsDAOImpl.java
@@ -1,5 +1,6 @@
 package org.parts.dao;
 
+import org.parts.dao.executor.Executor;
 import org.parts.model.Part;
 
 import java.util.ArrayList;
@@ -9,14 +10,17 @@ import java.util.List;
 
 public class PartsDAOImpl implements PartsDAO {
 
-/*    @PersistenceContext
-    private EntityManager em;*/
-
     private List<Part> parts = new ArrayList<>();
     {
         parts.add(new Part("10", "Flame thrower" ,"factory", 10, new GregorianCalendar(2017, 4, 7).getTime(), new GregorianCalendar(2017, 5, 6).getTime()));
         parts.add(new Part("50", "Rocket launcher" ,"factory", 15, new GregorianCalendar(2016, 4, 7).getTime(), new GregorianCalendar(2012, 9, 7).getTime()));
         parts.add(new Part("60", "Gas splitter" ,"factory", 18, new GregorianCalendar(2017, 3, 8).getTime(), new GregorianCalendar(2018, 5, 7).getTime()));
+    }
+
+    private Executor executor;
+
+    public PartsDAOImpl () {
+        executor = new Executor(ConnectionBuilder.getH2Connection());
     }
 
     @Override
@@ -26,13 +30,11 @@ public class PartsDAOImpl implements PartsDAO {
             Part clonedPart = new Part(part.getPartNumber(),
                                        part.getPartName(),
                                        part.getVendor(),
-                                       new Integer(part.getQuantity()),
+                                       part.getQuantity(),
                                        new Date(part.getShipped().getTime()),
                                        new Date(part.getReceived().getTime()));
             clonedParts.add(clonedPart);
         }
         return clonedParts;
-
-        /*return em.createQuery("from Part", Part.class).getResultList();*/
     }
 }
